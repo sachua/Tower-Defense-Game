8e2f8e5d63a60c663b1ff2b8f7aa9417841609fc
Chua Song Ann <chuasongann@gmail.com>
2018-07-27 20:37:24 +0800

    Add files via upload

--- ENITIO18_FINALE.ino
+++ ENITIO18_FINALE.ino
@@ -4,33 +4,37 @@
 #include <IRremote.h>
 #include <Adafruit_NeoPixel.h>
 #include <EEPROMex.h>
+#include <math.h>
+
+//TODO: Standardise DPS, DEATHPULSE, ATTACK codes from clans
 
 #define LDR_PIN A0
 #define HEALTH_PIN 5
 #define BUZZER_PIN 8
 #define ACTIVATED_PIN 4
 #define IR_PIN 11
+#define RESET_PIN 13
 #define BRIGHTNESS 50
 #define LIFE 50           //no. of pixels showing damage ratio
 #define ACTIVATED 50      //no. of pixels showing tower is activated
 #define DPS 2901          //Damage per second code
 #define DEATHPULSE 2900   //Instant-death code
-#define ATTACKED_R1 2704  //Weakest attack
-#define ATTACKED_R2 2705  //Weak attack
-#define ATTACKED_R3 2706  //Strong attack
-#define ATTACKED_R4 2707  //Strongest attack
-#define ATTACKED_G1 2708
-#define ATTACKED_G2 2709
-#define ATTACKED_G3 2710
-#define ATTACKED_G4 2711
-#define ATTACKED_B1 2712
-#define ATTACKED_B2 2713
-#define ATTACKED_B3 2714
-#define ATTACKED_B4 2715
-#define ATTACKED_Y1 2716
-#define ATTACKED_Y2 2717
-#define ATTACKED_Y3 2718
-#define ATTACKED_Y4 2719
+#define ATTACKED_R1 2601  //Weakest attack
+#define ATTACKED_R2 2602  //Weak attack
+#define ATTACKED_R3 2603  //Strong attack
+#define ATTACKED_R4 2801  //Strongest attack 2604
+#define ATTACKED_G1 2604
+#define ATTACKED_G2 2704
+#define ATTACKED_G3 2904
+#define ATTACKED_G4 2804
+#define ATTACKED_B1 2901
+#define ATTACKED_B2 2902
+#define ATTACKED_B3 2903
+#define ATTACKED_B4 2802 //2904
+#define ATTACKED_Y1 2701
+#define ATTACKED_Y2 2702
+#define ATTACKED_Y3 2703
+#define ATTACKED_Y4 2803 //2704
 #define dmg1 1            //Weakest attack damage
 #define dmg2 2            //weak attack damage
 #define dmg3 5            //Strong attack damage
@@ -43,7 +47,8 @@ IRrecv irrecv(IR_PIN);
 IRsend irsend;
 decode_results results;
 
-int addr, threshold, R, G, B, Y, avgR, avgG, avgB, avgY, dmgR, dmgG, dmgB, dmgY;
+int addr = 0, threshold, avgR = 0, avgG = 0, avgB = 0, avgY = 0;
+float R = 0, G = 0, B = 0, Y = 0;
 unsigned long previousMillis1 = 0, previousMillis2 = 0, previousMillis3 = 0;
 unsigned long red = Health_Pixels.Color(150, 0, 0), green = Health_Pixels.Color(0, 150, 0), blue = Health_Pixels.Color(0, 0, 150), yellow = Health_Pixels.Color(150, 150, 0);
 
@@ -118,6 +123,24 @@ void deactivated()
   interrupts();
 }
 
+//Read Scores
+void readScores()
+{
+  #if DEBUG
+  Serial.println("Reading Past Scores");
+  Serial.println("-------------------------------------------------------------");
+  #endif
+
+  R = EEPROM.readInt(addr);
+  addr += sizeof(int);
+  G = EEPROM.readInt(addr);
+  addr += sizeof(int);
+  B = EEPROM.readInt(addr);
+  addr += sizeof(int);
+  Y = EEPROM.readInt(addr);
+  addr = 0;
+}
+
 //Update Scores
 void updateScores(int A, int B, int C, int D)
 {
@@ -128,11 +151,11 @@ void updateScores(int A, int B, int C, int D)
   #endif
 
   EEPROM.updateInt(addr, A);
-  addr += sizeof(A);
+  addr += sizeof(int);
   EEPROM.updateInt(addr, B);
-  addr += sizeof(B);
+  addr += sizeof(int);
   EEPROM.updateInt(addr, C);
-  addr += sizeof(C);
+  addr += sizeof(int);
   EEPROM.updateInt(addr, D);
   addr = 0;
 }
@@ -142,6 +165,7 @@ void setup()
   pinMode(LDR_PIN, INPUT);
   pinMode(IR_PIN, INPUT);
   pinMode(BUZZER_PIN, OUTPUT);
+  pinMode(RESET_PIN, INPUT_PULLUP);
   pinMode(HEALTH_PIN, OUTPUT);
   pinMode(ACTIVATED_PIN, OUTPUT);
 
@@ -171,19 +195,15 @@ void setup()
   #endif
   #ifdef RED
     colorWipe(red, 50);
-    R = 1000;
   #endif
   #ifdef GREEN
     colorWipe(green, 50);
-    G = 1000;
   #endif
   #ifdef BLUE
     colorWipe(blue, 50);
-    B = 1000;
   #endif
   #ifdef YELLOW
     colorWipe(yellow, 50);
-    C = 1000;
   #endif
   #if DEBUG
     Serial.println("HealthBar filled");
@@ -200,35 +220,67 @@ void setup()
     Serial.println("-------------------------------------------------------------");
   #endif
 
-  addr = EEPROM.getAddress(sizeof(int));
+  addr = EEPROM.getAddress(sizeof(int));  //Initialise the addr for Arduino hard drive memory
+
+  readScores(); //Read past scores
 }
 
 void loop()
 {
-  int value = isShot(LDR_PIN, threshold);
+  bool value = isShot(LDR_PIN, threshold);
   unsigned long currentMillis1 = millis(), currentMillis2 = millis(), currentMillis3 = millis();
   const unsigned resultLength = results.bits;
 	const unsigned resultsValue = results.value;
 
+  if (!digitalRead(RESET_PIN))
+  {
+    #if DEBUG
+      Serial.println("RESET");
+    #endif
+    #ifdef RED
+      R = 1000;
+      G = 0;
+      B = 0;
+      Y = 0;
+    #endif
+    #ifdef GREEN
+      R = 0;
+      G = 1000;
+      B = 0;
+      Y = 0;
+    #endif
+    #ifdef BLUE
+      R = 0;
+      G = 0;
+      B = 1000;
+      Y = 0;
+    #endif
+    #ifdef YELLOW
+      R = 0;
+      G = 0;
+      B = 0;
+      Y = 1000;
+    #endif
+  }
+
   //When no laser hitting LDR
   if (value == 0)
   {
-    deactivated();
+    deactivated(); //deactivate the white neopixels
   }
 
   //When laser is hitting LDR
   //Incur damage when shot according to type of attack
   if (value == 1)
   {
-    activated();
+    activated();  //light up the white neopixels to show that it can now be attacked
     if (irrecv.decode(&results))
     {
-      const unsigned resultLength = results.bits;
+      const unsigned resultsLength = results.bits;
 		  const unsigned resultsValue = results.value;
       #if DEBUG
         Serial.println();
-        Serial.print("DMG received: ");
-        Serial.println(DMG);
+        Serial.print("DMG received! ");
         Serial.print("Damage code: ");
         Serial.println(resultsValue);
         Serial.println("-------------------------------------------------------------");
@@ -240,67 +292,51 @@ void loop()
         {
           case ATTACKED_R1:
             R += dmg1;
-            dmgR += dmg1;
             break;
           case ATTACKED_G1:
             G += dmg1;
-            dmgG += dmg1;
             break;
           case ATTACKED_B1:
             B += dmg1;
-            dmgB += dmg1;
             break;
           case ATTACKED_Y1:
             Y += dmg1;
-            dmgY += dmg1;
             break;
           case ATTACKED_R2:
             R += dmg2;
-            dmgR += dmg2;
             break;
           case ATTACKED_G2:
             G += dmg2;
-            dmgG += dmg2;
             break;
           case ATTACKED_B2:
             B += dmg2;
-            dmgB += dmg2;
             break;
           case ATTACKED_Y2:
             Y += dmg2;
-            dmgY += dmg2;
             break;
           case ATTACKED_R3:
             R += dmg3;
-            dmgR += dmg3;
             break;
           case ATTACKED_G3:
             G += dmg3;
-            dmgG += dmg3
             break;
           case ATTACKED_B3:
             B += dmg3;
-            dmgB += dmg3;
             break;
           case ATTACKED_Y3:
             Y += dmg3;
-            dmgY += dmg3;
             break;
           case ATTACKED_R4:
             R += dmg4;
-            dmgR += dmg4;
             break;
           case ATTACKED_G4:
             G += dmg4;
-            dmgG += dmg4;
             break;
           case ATTACKED_B4:
             B += dmg4;
-            dmgB += dmg4;
             break;
           case ATTACKED_Y4:
             Y += dmg4;
-            dmgY += dmg4;
             break;
           default:
             delay(50);
@@ -311,10 +347,10 @@ void loop()
   }
 
   //Ratio of damage dealt
-  avgR == round((R/(R+G+B+Y))*LIFE);
-  avgG == round(((G/(R+G+B+Y))*LIFE)+avgR);
-  avgB == round(((B/(R+G+B+Y))*LIFE)+avgR+avgG);
-  avgY == round(((Y/(R+G+B+Y))*LIFE)+avgR+avgG+avgB);
+  avgR = roundf((R/(R+G+B+Y))*LIFE);
+  avgG = roundf(((G/(R+G+B+Y))*LIFE)+avgR);
+  avgB = roundf(((B/(R+G+B+Y))*LIFE)+avgG);
+  avgY = roundf(((Y/(R+G+B+Y))*LIFE)+avgB);
 
   //Tower attacking
   if (currentMillis1 - previousMillis1 >= 1000)
@@ -346,7 +382,7 @@ void loop()
     previousMillis2 = currentMillis2;
 
     //Buzzer warning
-    for (x = 0; x < 15; x++)
+    for (int x = 0; x < 20; x++)
     {
       digitalWrite(BUZZER_PIN, HIGH);
       delay(50);
@@ -373,22 +409,21 @@ void loop()
 
   //Neopixels depict ratio of damage dealt by clan
   noInterrupts();
-
-  for (int i = 0 ; i < avgR ; i++)
+  for (int i = 0 ; i < LIFE ; i++)
   {
-    Health_Pixels.setPixelColor(i, red);
-  }
-  for (int i = avgR ; i < avgG ; i++)
-  {
-    Health_Pixels.setPixelColor(i, green);
-  }
-  for (int i = avgG ; i < avgB ; i++)
-  {
-    Health_Pixels.setPixelColor(i, blue);
-  }
-  for (int i = avgB ; i < avgY ; i++)
-  {
-    Health_Pixels.setPixelColor(i, yellow);
+    if (i < avgR)
+    {
+      Health_Pixels.setPixelColor(i, red);
+    }
+    else if (i<avgG){
+      Health_Pixels.setPixelColor(i, green);
+    }
+    else if(i<avgB){
+      Health_Pixels.setPixelColor(i, blue);
+    }
+    else{
+      Health_Pixels.setPixelColor(i, yellow);
+    }
   }
 
   Health_Pixels.show();
@@ -398,16 +433,15 @@ void loop()
   irrecv.enableIRIn();
 
   #if DEBUG
-    Serial.println();
     Serial.println("SCORES");
-    Serial.print("RED : ");
-    Serial.println(EEPROM.readInt(addr));
-    Serial.print("GREEN : ");
-    Serial.println(EEPROM.readInt(addr + sizeof(R)));
-    Serial.print("BLUE : ");
-    Serial.println(EEPROM.readInt(addr + sizeof(G)));
-    Serial.print("YELLOW : ");
-    Serial.println(EEPROM.readInt(addr + sizeof(B)));
+    Serial.print("Number of red pixels: ");
+    Serial.println(avgR);
+    Serial.print("Number of green pixels: ");
+    Serial.println(avgG - avgR);
+    Serial.print("Number of blue pixels: ");
+    Serial.println(avgB - avgG);
+    Serial.print("Number of yellow pixels: ");
+    Serial.println(avgY - avgB);
     Serial.println("-------------------------------------------------------------");
   #endif
 
@@ -415,6 +449,6 @@ void loop()
   if (currentMillis3 - previousMillis3 >= 90000)
   {
     previousMillis3 = currentMillis3;
-    updateScores(dmgR, dmgG, dmgB, dmgY);
+    updateScores(R, G, B, Y);
   }
 }
